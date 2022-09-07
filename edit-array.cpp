24022fe724e204b568b15f33c6c05ec4c1409f61
Ashlee Tolentino <ashleetolentino@Ashlees-MacBook-Air.local>
2022-09-07 12:10:24 -0400

    finished code for Lab2C

--- edit-array.cpp
+++ edit-array.cpp
@@ -20,7 +20,6 @@ void printArray(int myData[10])
 int main()
 {
 	int myData[10];
-
 	for(int i = 0; i < 10; i++)
 	{
 		myData[i] = 1;
@@ -28,22 +27,18 @@ int main()
 
 	int i;
 	int v;
-
 	do
 	{
 		printArray(myData);
 		std::cout << "\nInput index: " << std::endl;
-		//int i;
 		std::cin >> i;
 
 		std::cout << "Input value: " << std::endl;
-		//int v;
 		std::cin >> v;
 
 		myData[i] = v;
 	}
 	while(i >= 0 && i < 10);
-
 	std::cout << "Index out of range. Exit." << std::endl;
 	
 	return 0;
