9dd93dee2348ea9e26c8a9e75222216f69f7d2e1
Ashlee Tolentino <ashleetolentino@Ashlees-MacBook-Air.local>
2022-09-07 11:23:33 -0400

    finished funcs.h

---
+++ funcs.h
@@ -0,0 +1,11 @@
+//.H FILES ARE FOR FILES THAT ARE NOT MAIN.CPP FILES; INCLUDE .H FILES INSIDE THESE OTHER FILES
+
+//#pragma once //ONLY add .h file ONCE - 1st way
+
+#ifndef MATHFUNCS_H //if MATHFUNCS_H is NOT defined - 2nd way and it's usually better
+#define MATHFUNCS_H 
+
+//FUNCTION SIGNATURES
+void print_interval(int, int);
+
+#endif
