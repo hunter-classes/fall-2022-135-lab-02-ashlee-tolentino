dd76d2a2a01dd92965869b9167de1a2603e58b25
Ashlee Tolentino <ashleetolentino@Ashlees-MacBook-Air.local>
2022-09-07 11:19:30 -0400

    finished main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab2B
+
+This program ... TYPE HERE
+*/
+
+//How to do multiple compilations
+//Include 3 .h files for 3 .cpp files inside them
+
+#include <iostream>
+#include "funcs.h"
+
+void print_interval(int, int);
+
+
+int main()
+{
+	print_interval(0, 20);
+	return 0;
+}
