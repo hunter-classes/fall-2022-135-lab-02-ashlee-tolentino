43c6b8f50e6af9997a5f3a3ab9dc9c07f6cc67b5
Ashlee Tolentino <ashleetolentino@Ashlees-MacBook-Air.local>
2022-09-08 09:21:38 -0400

    fixed output format

--- valid.cpp
+++ valid.cpp
@@ -11,17 +11,17 @@ This program asks the user to input an integer in the range 0 < n < 100. If the 
 
 int main() 
 {
-	std::cout << "Please enter an integer: " << std::endl;
+	std::cout << "Please enter an integer: ";
 	int n;
 	std::cin >> n;
 	
 	while (n <= 0 || n >= 100)
 	{
-		std::cout << "Please re-enter: " << std::endl;
+		std::cout << "Please re-enter: ";
 		std::cin >> n;
 	}
 
-	std::cout << "Number squared is " << n * n << std::endl;
+	std::cout << "\nNumber squared is " << n * n << std::endl;
 
 	return 0;
 }
